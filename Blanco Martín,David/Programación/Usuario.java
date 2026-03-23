5f5bcc829eb7e833a3ab0f57df10a9f46739753c
David <davidblancomartin@alumnos.tuniversformacion.es>
2026-03-23 12:23:39 +0100

    Creación clase Usuario

---
+++ Blanco Martín,David/Programación/Usuario.java
@@ -0,0 +1,32 @@
+public class Usuario {
+    private String nombre;
+    private String email;
+    private String password;
+ 
+    public Usuario(String nombre, String email, String password) {
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+    }
+ 
+    // Getters
+    public String getNombre()
+     { return nombre; }
+    public void setNombre(String nombre)
+     { this.nombre = nombre; }
+    public String getEmail() 
+    { return email; }
+
+    // Setters
+    public void setEmail(String email) 
+    { this.email = email; }
+    public String getPassword()
+     { return password; }
+    public void setPassword(String password)
+     { this.password = password; }
+ 
+    
+   public String toString() {
+    return "nombre: " + nombre + " | Email: " + email;
+}
+}
