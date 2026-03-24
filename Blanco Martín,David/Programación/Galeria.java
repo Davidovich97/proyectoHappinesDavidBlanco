a362b751745f80ecb566bd0ebc6d85012a583b76
David <davidblancomartin@alumnos.tuniversformacion.es>
2026-03-24 13:28:51 +0100

    Modificación de archivo y carpeta del repositorio

---
+++ Blanco Martín,David/Programación/Galeria.java
@@ -0,0 +1,26 @@
+public class Galeria {
+    private int id;
+    private String titulo;
+    private int idEvento;
+ 
+    public Galeria(int id, String titulo, int idEvento) {
+        this.id = id;
+        this.titulo = titulo;
+        this.idEvento = idEvento;
+    }
+ 
+    // Getters
+    public int getId() { return id; }
+    public String getTitulo() { return titulo; }
+    public int getIdEvento() { return idEvento; }
+
+    // Setters
+    public void setId(int id) { this.id = id; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public void setIdEvento(int idEvento) { this.idEvento = idEvento; }
+ 
+    @Override
+    public String toString() {
+        return "id" + id + " titulo" + titulo + " idEvento" + idEvento;
+    }
+}
