fbafb8b4cec95f85dabcff592286e3a78ae382ee
Davizon22 <zipizapeds@gmail.com>
2026-03-27 09:06:47 +0100

    Modificación de la cantidad de caracteres permitidos en los campos de las tablas Usuarios y Eventos

--- Blanco Martín,David/Base de datos/Base de Datos Happiness.sql
+++ Blanco Martín,David/Base de datos/Base de Datos Happiness.sql
@@ -3,15 +3,15 @@ USE Happiness;
 
 CREATE TABLE Usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    nombre VARCHAR(60) NOT NULL,
+    email VARCHAR(60) UNIQUE NOT NULL,
+    password VARCHAR(60) NOT NULL
 );
 
 CREATE TABLE Eventos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     fecha DATE NOT NULL,
     titulo VARCHAR(150) NOT NULL,
-    ubicacion VARCHAR(255) NOT NULL,
-    descripcion TEXT
+    ubicacion VARCHAR(80) NOT NULL,
+    descripcion VARCHAR(1000)
 );
